6f93ac6ee34bb4c74139a6df0bac4615a4928c23
byxz885@gmail.com <53200767+byzESC@users.noreply.github.com>
2023-01-23 17:29:34 -0800

    finishing license functions

--- index.js
+++ index.js
@@ -1,43 +1,42 @@
 // TODO: Include packages needed for this application
-// installed inquirer with npm
-
-// input: a bunch of user text inputs
-// output: a readme
-// exception: nothing input or empty spaces .trim()
-
 const fs = require('fs');
-const generateMD = require('./utils/generateMarkdown');
+const generateMD = require('./utils/generateMarkdown.js');
 const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
 const questions = ['What is your GitHub username?', 'What is your email address?', 'What is your project\'s name?', 'Please write a short description of your project', 'What kind of license should your project have?', 'What command should be run to install dependencies?', 'What command should be run to run tests?', 'What does the user need to know about using the repo?', 'What does the user need to know about contributing to the repo?'];
 
-const [userName, email, projectName, description, license, dependenciesCommand, testCommand, repoInfo, contribution] = questions;
+const [userName, email, projectName, description, license, dependencies, testCommand, repoInfo, contribution] = questions;
+const licenseChoices = ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'none'];
 
-console.log(userName);
 // TODO: Create a function to write README file
+// Acceptances Criteria --- WHEN I am prompted for information about my application repository --- THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
 function writeToFile(fileName, data) {
-
+    fs.appendFile(fileName, data, (err) => {
+        err ? console.log(err) : console.log(data);
+        console.log('generating a README...');
+    });
 }
 
 // TODO: Create a function to initialize app
     // get all input and generate a readme, THEN pass to writeToFile
 function init() {
-    let readmeResources = {}
-
-// ask the questions
-// wait for input
-// store questions[i] as key, and input as value
-
-    inquirer
-        .prompt (questions)
-        .then((answer) => {
-            console.log(answer);
-        })
-
+    inquirer.prompt([
+        { type: 'input',  name:'userName',      message: userName },
+        { type: 'input',  name:'email',         message: email },
+        { type: 'input',  name:'projectName',   message: projectName },
+        { type: 'input',  name:'description',   message: description },
+        { type: 'list',   name:'license',       message: license,           choices: licenseChoices },
+        { type: 'input',  name:'dependencies',  message: dependencies,      default: 'npm i' },
+        { type: 'input',  name:'testCommand',   message: testCommand,       default: 'npm test'},
+        { type: 'input',  name:'repoInfo',      message: repoInfo },
+        { type: 'input',  name:'contribution',  message: contribution }
+    ]).then(response => {
+        let currentMD = generateMD.generateMarkdown(response);
+        // Acceptances Criteria --- WHEN I enter my project title --- THEN this is displayed as the title of the README
+        writeToFile(`${response.projectName}-README.md`, currentMD);
+    });
 }
 
 // Function call to initialize app
 init();
-
-console.log(generateMD.renderLicenseLink(1));
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,33 +1,108 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 
+// Acceptances Criteria --- WHEN I choose a license for my application from a list of options
+// --- THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  let result = "";
-
-  return result;
+  if (license !== 'none') {
+    license = license.split(' ').join('');
+    return `https://img.shields.io/badge/LICENSE-${license}-success`;
+  } else {
+    return "";
+  }
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  let result = "hello";
-
-  return result;
+  if (license === 'MIT') {
+    return 'https://choosealicense.com/licenses/mit/';
+  } else if (license === 'APACHE 2.0') {
+    return 'https://choosealicense.com/licenses/apache-2.0/';
+  } else if (license === 'GPL 3.0') {
+    return 'https://choosealicense.com/licenses/gpl-3.0/';
+  } else if (license === 'BSD 3') {
+    return 'https://choosealicense.com/licenses/bsd-3-clause/';
+  } else {
+    return "";
+  }
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  let result = "";
-
-  return result;
+  if (license !== 'none') {
+    return `This project is licensed under the [${license}] license, you can
+[Read More](${renderLicenseLink(license)})`;
+  }else {
+    return "";
+  }
 }
+
+// Acceptances Criteria --- WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+// --- THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+
+// Acceptances Criteria --- WHEN I enter my GitHub username
+// --- THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+
+// Acceptances Criteria --- WHEN I enter my email address
+// --- THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+
+// Acceptances Criteria --- WHEN I click on the links in the Table of Contents
+// --- THEN I am taken to the corresponding section of the README
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.projectName}
+![${data.license}](${renderLicenseBadge(data.license)})
+
+## Description
+
+${data.description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+To install necessary dependencies, run the following command: 
+
+${data.dependencies}
+
+## Usage
+
+${data.repoInfo}
+
+## License
+
+${renderLicenseSection(data.license)}
+
+
+## Contributing
+
+${data.contribution}
+
+## Tests
+
+To run test, run the following command:
+
+
+${data.testCommand}
+
+
+## Questions
+
+If you have any questions about the repo, open an issue or contact me directly at ${data.email}. 
+
+You can find more of my work at [here](https://github.com/${data.userName}/).
 
 `;
 }
 
-module.exports = generateMarkdown.js;
+module.exports = { generateMarkdown };
